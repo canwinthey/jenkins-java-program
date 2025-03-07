defeb646990f07dd935a631d594f70a71300b640
Prasanna.Kanvinde <Prasanna.Kanvinde@Xoriant.com>
2025-03-07 12:18:35 +0530

    Jenkins.java file added

---
+++ Jenkins.java
@@ -0,0 +1,7 @@
+public class Jenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Prasanna!!");
+        System.out.println("Welcome to Jenkins-java-program!!");
+    }
+}
