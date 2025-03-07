03752d18ffb09a31231a8f47373f6e319e9acac5
Prasanna.Kanvinde <Prasanna.Kanvinde@Xoriant.com>
2025-03-07 12:29:28 +0530

    Jenkins.java class updated

--- Jenkins.java
+++ Jenkins.java
@@ -3,5 +3,8 @@ public class Jenkins {
     public static void main(String[] args) {
         System.out.println("Hello Prasanna!!");
         System.out.println("Welcome to Jenkins-java-program!!");
+        System.out.println("Added another file Student.java");
+        Student student = new Student();
+        System.out.println("name = " + student.getName());
     }
 }
